9aa5d09db9e58365d27ab00a79758664bf50263a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-11-21 01:41:06 -0800

    A disappointing prototype

--- PaperTrail/src/browser_action/js/main.js
+++ PaperTrail/src/browser_action/js/main.js
@@ -1,7 +1,28 @@
-var CompanyProfile = {
-	render: function() {
-		$('#target').html('THIS IS WORKING!');
-	}
-};
+// var CompanyProfile = {
+// 	render: function() {
+// 		alert('asdlfkj');
+// 		$('#target').html(Mustache.render($('#company-template', {
+// 			name: 'COMPANY NAME'
+// 		});
+// 	}
+// };
 
-CompanyProfile.render();
+// CompanyProfile.render();
+
+d3.selectAll("p").style("color", "red");
+
+// var sampleSVG = d3.select("#viz")
+//     .append("svg")
+//     .attr("width", 100)
+//     .attr("height", 100);   
+
+
+
+
+// Get the context of the canvas element we want to select
+var ctx = document.getElementById("myChart").getContext("2d");
+var myNewChart = new Chart(ctx).PolarArea(data);
+
+new Chart(ctx).PolarArea(data, options);
+
+var myDoughnutChart = new Chart(ctx[1]).Doughnut(data,options);
